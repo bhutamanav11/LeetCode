c11c6b061e07f9341102ad51e4539d5b39e57291
Manav Bhuta <108284638+bhutamanav11@users.noreply.github.com>
2025-06-22 12:48:22 +0530

    Update problem 1822 sign_of_product_of_array.cpp

--- Easy/sign_of_product_of_array.cpp
+++ Easy/sign_of_product_of_array.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int signFunc(int x){
-        if(x>0) return 1;
-        if(x<0) return -1;
+        if(x > 0) return 1;
+        if(x < 0) return -1;
         return 0;
-        }
-    int prod = 1;   
+    }
+
     int arraySign(vector<int>& nums) {
-        for (int i = 0;i<nums.size();i++){
-            prod = prod*nums[i];
+        int prod = 1;
+
+        for (int i = 0; i < nums.size(); i++) {
+            prod *= nums[i];
         }
+
         return signFunc(prod);
     }
 };
