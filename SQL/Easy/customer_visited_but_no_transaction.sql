8e0a3d62ffbf605d0f005742d38d5a7c399a6504
Manav Bhuta <108284638+bhutamanav11@users.noreply.github.com>
2025-06-24 12:59:10 +0530

    Create prob 1581 customer_visited_but_no_transaction.sql

---
+++ SQL/Easy/customer_visited_but_no_transaction.sql
@@ -0,0 +1,1 @@
+select customer_id , COUNT(*) as count_no_trans from Visits left outer join Transactions on Visits.visit_id  = Transactions.visit_id  where Transactions.transaction_id is NULL group by Visits.customer_id 
