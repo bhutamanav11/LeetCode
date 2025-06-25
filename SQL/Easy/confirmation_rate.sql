b2f3b7de712891d5d195113042a60d9d039ee7ea
Manav Bhuta <108284638+bhutamanav11@users.noreply.github.com>
2025-06-25 19:49:50 +0530

    Delete SQL/Easy/confirmation_rate.sql

--- SQL/Easy/confirmation_rate.sql
+++
@@ -1,8 +0,0 @@
-select 
-    s.user_id , 
-    IFNULL(round(sum(action='confirmed')/Count(*),2),0.00) as confirmation_rate 
-from Signups s
-    left join Confirmations c 
-    on
-        s.user_id = c.user_id
-    group by s.user_id;
