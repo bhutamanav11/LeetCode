f9109cf11b67fb1c01e6845e28e939dabc4ca8ee
Manav Bhuta <108284638+bhutamanav11@users.noreply.github.com>
2025-07-28 10:50:54 +0530

    Create problem 463 island-perimeter.cpp

---
+++ Easy/island-perimeter.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int perimeter =  0;
+        int row = grid.size();
+        int col = grid[0].size();
+        for(int r = 0;r<row;r++){
+            for(int c = 0;c<col;c++){
+                if(grid[r][c]==1){
+                    perimeter +=4;
+                
+                    // down neighbour
+                    if(r+1<row && grid[r+1][c]==1){
+                        perimeter-=2;
+                    }
+                    //right 
+                    if(c+1<col && grid[r][c+1]==1){
+                        perimeter-=2;
+                    }
+                }
+            }
+        }
+        return perimeter;
+    }
+};
