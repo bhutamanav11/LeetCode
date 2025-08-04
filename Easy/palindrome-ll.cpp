1278d4d8337180d70de0dad9ebf496e2506afd77
Manav Bhuta <108284638+bhutamanav11@users.noreply.github.com>
2025-08-04 11:12:16 +0530

    Create prob 234 palindrome-ll.cpp

---
+++ Easy/palindrome-ll.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (!head || !head->next) return true;
+        
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        ListNode* secondHalf = reverseList(slow);
+        ListNode* firstHalf = head;
+        
+        while (secondHalf) {
+            if (firstHalf->val != secondHalf->val) {
+                return false;
+            }
+            firstHalf = firstHalf->next;
+            secondHalf = secondHalf->next;
+        }
+        
+        return true;
+    }
+    
+private:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr) {
+            ListNode* nextTemp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;  
+    }
+};
